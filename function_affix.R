cbbd9b01e198ad8bae4ba2c73e81e215dd074b13
yang yang <44868838+yang0y@users.noreply.github.com>
2018-12-11 13:24:41 +0100

    Update function_affix.R

--- function_affix.R
+++ function_affix.R
@@ -1,25 +1,25 @@
-####Construire dataframe de suffixes de base des label
+####Construire dataframe de affix base de label
 #input
 #    a: tous les token
-#    b: label de token
+#    b: labell de token
 #output
-#   dataframe "suffix"  "sPROTEN" "sDNA"    "sRNA"    "sCLINE"   "sCTYPE"
-construt_suffix<-function(a, b){
+#   un vector de suffix qui est efficacite
+construt_suffix<-function(a,b){
   df_af <- data.frame(word=a,label=str_replace_all(b,"[BI-]",""),stringsAsFactors = FALSE)
-  #si le nombre de character du mot >=5 on fait le substr, ici j'ai essayé avec 4 gram de suffix et prefix 
+  #si le nombre de character du mot >=5 on fait le substr, ici j'ai essayé avec 4 gram de suffix et prefix
   suf5 <- ifelse(nchar(df_af$word)>=5 ,str_sub(df_af$word, -5),'non_non')
   ####Construire dataframe suffix(5 grams) avec weight####
   df_suf<-data.frame(suf5,df_af$label,stringsAsFactors = FALSE)
-  #enlever les mots de moins de 5 caractere et suffix contient '-'
+  #enlever le mot moins 5 caractere et suffix contient '-'
   df_suf<-df_suf[df_suf$suf5 != "non_non" & is.na(str_extract(df_suf$suf5, "-"))==TRUE,]
-  # Calcul des frequences de suffix
+  #compte frequence de suffix
   F_suf<-as.data.frame(table(df_suf))
-  # On ordonne les fréquences par ordre décroissant
+  #mettre ordre descendre
   F_suf<-F_suf[order(-F_suf$Freq),]
-  # On garde les 100 affixes qui occurent le plus
+  #chercher 100 plus frequence suffix qui est entitee
   F_suf_100<-F_suf[which(F_suf$df_af.label!="O"),]
   suf_100<-unique(F_suf_100$suf5)[1:100]
-  # Calcul des "weight" par formule (in[]-out[])/(in[]+out[])
+  #calculer weight par formule (in[]-out[])/(in[]+out[])
   weight_suf<-rep(0, 100)
   j<-1
   for(i in suf_100){
@@ -29,28 +29,21 @@ construt_suffix<-function(a, b){
     j<-j+1
   }
   df_weight_suf<-data.frame(suf_100,weight_suf)
-  #on garde les weight > 0.7 (c.f. article)
+  #on prend weight superieur 0.5
   df_weight_suf<-df_weight_suf[weight_suf>=0.7,]
   #construire comme dictionary de labels
-  s_p<-sapply(df_weight_suf[,1], function(x) subset(F_suf[F_suf$suf5==x,],df_af.label=="PROTEN",select = Freq)$Freq/sum(F_suf[F_suf$suf5==x,]$Freq))
-  s_d<-sapply(df_weight_suf[,1], function(x) subset(F_suf[F_suf$suf5==x,],df_af.label=="DNA",select = Freq)$Freq/sum(F_suf[F_suf$suf5==x,]$Freq))
-  s_r<-sapply(df_weight_suf[,1], function(x) subset(F_suf[F_suf$suf5==x,],df_af.label=="RNA",select = Freq)$Freq/sum(F_suf[F_suf$suf5==x,]$Freq))
-  s_c<-sapply(df_weight_suf[,1], function(x) subset(F_suf[F_suf$suf5==x,],df_af.label=="CLINE",select = Freq)$Freq/sum(F_suf[F_suf$suf5==x,]$Freq))
-  s_t<-sapply(df_weight_suf[,1], function(x) subset(F_suf[F_suf$suf5==x,],df_af.label=="CTYPE",select = Freq)$Freq/sum(F_suf[F_suf$suf5==x,]$Freq))
-  df_labs<-data.frame(suffix=df_weight_suf[,1],sPROTEN=s_p,sDNA=s_d,sRNA=s_r,sCLINE=s_c,sCTYPE=s_t,stringsAsFactors=FALSE)
-  return(df_labs)
+  return(df_weight_suf[,1])
 }
-
 
 ####Construire dataframe de préfixes de base des label
 #input
 #    a: tous les token
 #    b: label de token
 #output
-#   dataframe "prefix"  "pPROTEN" "pDNA"    "pRNA"    "pCLINE"   "pCTYPE" 
+#   un vector de prefix qui est efficacite 
 construt_prefix<-function(a,b){
   df_af <- data.frame(word=a,label=str_replace_all(b,"[BI-]",""),stringsAsFactors = FALSE)
-  #si le nombre de character du mot >=5 on fait le substr, ici j'ai essayé avec 4 gram de suffix et prefix 
+  #si le nombre de character du mot >=5 on fait le substr, ici j'ai essayé avec 4 gram de suffix et prefix
   pre4 <- ifelse(nchar(df_af$word)>=5 ,str_sub(df_af$word, 1,4),'non_non')
   ####Construire dataframe prefix(4 grams) avec weight####
   df_pref<-data.frame(pre4,df_af$label,stringsAsFactors = FALSE)
@@ -69,38 +62,40 @@ construt_prefix<-function(a,b){
   }
   df_weight_pref<-data.frame(pref_100,weight_pref)
   df_weight_pref<-df_weight_pref[weight_pref>=0.7,]
-  p_p<-sapply(df_weight_pref[,1], function(x) subset(F_pref[F_pref$pre4==x,],df_af.label=="PROTEN",select = Freq)$Freq/sum(F_pref[F_pref$pre4==x,]$Freq))
-  p_d<-sapply(df_weight_pref[,1], function(x) subset(F_pref[F_pref$pre4==x,],df_af.label=="DNA",select = Freq)$Freq/sum(F_pref[F_pref$pre4==x,]$Freq))
-  p_r<-sapply(df_weight_pref[,1], function(x) subset(F_pref[F_pref$pre4==x,],df_af.label=="RNA",select = Freq)$Freq/sum(F_pref[F_pref$pre4==x,]$Freq))
-  p_c<-sapply(df_weight_pref[,1], function(x) subset(F_pref[F_pref$pre4==x,],df_af.label=="CLINE",select = Freq)$Freq/sum(F_pref[F_pref$pre4==x,]$Freq))
-  p_t<-sapply(df_weight_pref[,1], function(x) subset(F_pref[F_pref$pre4==x,],df_af.label=="CTYPE",select = Freq)$Freq/sum(F_pref[F_pref$pre4==x,]$Freq))
-  df_labs<-data.frame(prefix=df_weight_pref[,1],pPROTEN=p_p,pDNA=p_d,pRNA=p_r,pCLINE=p_c,pCTYPE=p_t,stringsAsFactors=FALSE)
-  return(df_labs)
+  return(df_weight_pref[,1])
 }
 
 ###find affix par word###
 #input
-#    w:word token
-#    pref:dataframe de prefix
-#    suf:dataframe de suffix
+#    words:word token
+#    pref:un vector de prefix
+#    suf:un vector de suffix
+#output
+#    un dataframe 3 colonne (word,prefix,suffix)
+#    si le prefix/suffix de word est dans le vector prefix/suffix, on mets TRUE dans cette colonne
+#    sion FALSE
 find_Affix<-function(words,pref,suf){
-  df_af<-data.frame()
-  for(w in words){
-    if(nchar(w)<5) df_af<-rbind(df_af,rep(0,10))
+  require(hash)
+  h<-hash()
+  uniq_word<-unique(words)
+  for(w in uniq_word){
+    if(nchar(w)<5) .set(h, keys = w, values = rep(FALSE,2))
     else{
-      w_suf<-str_sub(w, -5)
-      w_pref<-str_sub(w, 1,4)
-      if(length(grep(w_suf, suf[,1]))==0) wsuf <- rep(0,5)
-      else wsuf <- suf[grep(w_suf,suf[,1]),2:6]
-      if(length(grep(w_pref, pref[,1]))==0) wpref<-rep(0,5)
-      else wpref<-suf[grep(w_pref,pref[,1]),2:6]
-      df_af<-rbind(df_af,c(wpref,wsuf))
+      w_suf<-w %>%
+        str_sub(-5)
+      w_pref<-w %>%
+        str_sub(1,4)
+
+      if(length(grep(w_suf, suf))==0) wsuf <- FALSE
+      else wsuf <- TRUE#suf[grep(w_suf,suf[,1]),2]
+      if(length(grep(w_pref, pref))==0) wpref<-FALSE
+      else wpref<-TRUE#suf[grep(w_pref,pref[,1]),2]
+      .set(h, keys = w, values = c(as.character(wpref),as.character(wsuf)))
     }
   }
-  colnames(df_af)<-c("pPROTEN","pDNA",",pRNA","pCLINE","pCTYPE","sPROTEN","sDNAL","sRNA","sCLINE","sCTYPE")
-  return(data.frame(words,df_af,stringsAsFactors=FALSE))
+  df_af<-foreach(w=words,.combine = "rbind")%do% c(w,values(h, w))
+  
+  colnames(df_af)<-c("word","prefix","suffix")
+  return(df_af)
 }
-tmp_p<-construt_prefix(a,b)
-tmp_s<-construt_suffix(a,b)
-tmp<-find_Affix(c("transcription","curcumin"),tmp_p,tmp_s)
-View(tmp)
+
